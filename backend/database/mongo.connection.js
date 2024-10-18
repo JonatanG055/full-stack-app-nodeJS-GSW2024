a131c996dd02d5b6e80f509c74b997b0cf9eceef
JonatanG055 <jonataneliasguevarachicas129@gmail.com>
2024-10-18 15:35:01 -0600

    implentacion de conexion a base de datos y dokerizacion

--- backend/database/mongo.connection.js
+++ backend/database/mongo.connection.js
@@ -1,26 +1,17 @@
+import dotenv from "dotenv"; // Cambié la importación
 import mongoose from "mongoose";
-import { configDotenv } from "dotenv";
 
-configDotenv()
+dotenv.config(); // Cargar las variables de entorno
 
-
-let URLDATABASE = process.env.mongourl
-
+let URLDATABASE = process.env.MONGOURL; // Cambiado a MONGOURL
 
 const mongoconnect = async () => {
-    
     try {
-        
-        await mongoose.connect(URLDATABASE)
-
-        console.log("conexion exitosa a la base de datos");
-        
-
+        await mongoose.connect(URLDATABASE); // Usar la URL de la base de datos
+        console.log("Conexión exitosa a la base de datos");
     } catch (error) {
-        console.error(error);
-        
+        console.error("Error en la conexión a la base de datos:", error); // Mensaje de error más descriptivo
     }
+};
 
-}
-
-export default mongoconnect
+export default mongoconnect;
--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,20 @@
+import cors from "cors";
+import dotenv from "dotenv"; // Cambiado a dotenv
 import express from "express";
-import dotenvConfig from "dotenv";
 import mongoconnect from "./database/mongo.connection.js";
 import Routes from "./routers/product.routes.js";
-import cors from "cors"
-let port = 3000
 
-const app = express()
-mongoconnect()
-app.use(cors())
+dotenv.config(); // Asegúrate de cargar las variables de entorno
 
-app.use(express.json())
-app.use("/api", Routes)
+let port = 3000;
 
-app.listen(port, () =>{console.log(`servidor corriendo en http://localhost:${port}`);
+const app = express();
+mongoconnect(); // Llama a la función de conexión
 
-})
+app.use(cors());
+app.use(express.json());
+app.use("/api", Routes);
+
+app.listen(port, () => {
+    console.log(`servidor corriendo en http://localhost:${port}`);
+});
